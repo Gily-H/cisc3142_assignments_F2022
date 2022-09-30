20875ce39ff2f810b7ca431f0bca0e538e926d01
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-09-30 00:22:14 -0400

    complete source code for lab4 nutrition calculator exercise

--- lab4/src/nutrition_calculator.cpp
+++ lab4/src/nutrition_calculator.cpp
@@ -1,29 +1,59 @@
+/*
+ * Program to determine the number of soda cans with artificial sweetener
+ * that a person can drink before dying
+ */
+
 #include <iostream>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
+using std::setprecision;
+using std::fixed;
 using std::endl;
 
-void lethal_dose() {
-  // lethal dose for a mouse: 5grams
-  // mouse mass: 35 grams
-  // 5 grams / 35 gram mouse = lethal dose (1/7th of body mass)
+void calculate_lethal_dose() {
+  cout << "Find out how much soda you would need to drink in order to perish" << endl;
+  // amount of artificial sweetener needed to kill a mouse in grams
+  const double mouse_sweetener = 5.0;
+  // mouse mass reference in grams
+  const double mouse_mass = 35.0;
+  // proportion of sweetener to mass needed to be lethal
+  const double mouse_lethal = mouse_sweetener / mouse_mass;
+  // 100lb person mass reference in grams 
+  const double person_mass = 45400.0;
+  // soda-can mass in grams
+  const double soda_mass = 350.0;
+  // amount of sweetener in a soda can
+  const double sweetener = 0.001;
 
-  // 100lb person mass: 45400 grams 
-  // soda can mass: 350 grams
-  // amount of sweetener in soda can: 1/10
+  // user-inputted desired weight after dieting
+  double desired_weight;
+  double weight_in_grams;
+  double person_lethal;
+  double soda_lethal;
+  int soda_cans;
+  cout << "Please enter your desired weight:" << endl;
+  // loop until no more input
+  while (cin >> desired_weight) {
+	  // convert the desired weight into grams based on hundred-pound person ratio
+	  weight_in_grams = (desired_weight / 100) * person_mass;
+	  // calculate lethal dose of sweetener in grams needed for person, based on mouse lethal proportion
+	  person_lethal = weight_in_grams * mouse_lethal;
+	  // find lethal dose in grams of soda
+	  soda_lethal = person_lethal / sweetener;
+	  // find lethal dose in cans of soda
+	  soda_cans = soda_lethal / soda_mass;
 
-  // desired weight after dieting
-  unsigned desired_weight;
-  // while (user still has input)
-    cout << "Enter the desired weight after dieting:" << endl;
-    cin >> desired_weight;
-    
-    // convert desired weight into grams
-    // multiply the desired weight in grams by 1/7 to get the lethal dose
-    // divide the lethal dose by mass of soda cans = number of soda cans to lethal
+	  cout << "Lethal dose of sweetener for " << desired_weight << "lbs person: " << person_lethal << "grams of sweetener" << endl;
+	  cout << setprecision(2) << fixed << "You would need to drink about " << soda_cans << " cans or about " << soda_lethal << " grams of soda before you would die trying to diet to " << desired_weight << "lbs" << endl;
+
+	  cout << endl;
+	  cout << "Enter a new weight or exit the program using 'Ctrl+Z' or 'Ctrl+D'" << endl;
+  }
 }
 
 int main() {
+	calculate_lethal_dose();
   return 0;
 }
