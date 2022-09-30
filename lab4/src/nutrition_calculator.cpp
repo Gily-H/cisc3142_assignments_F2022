d025701623dc5b7dd26cd4ffc688c309a89e8c19
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-09-30 11:44:19 -0400

    implement logic to rerun the program in more user-friendly manner

--- lab4/src/nutrition_calculator.cpp
+++ lab4/src/nutrition_calculator.cpp
@@ -5,12 +5,14 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using std::cin;
 using std::cout;
-using std::setprecision;
-using std::fixed;
 using std::endl;
+using std::fixed;
+using std::setprecision;
+using std::string;
 
 void calculate_lethal_dose() {
   cout << "Find out how much soda you would need to drink in order to perish" << endl;
@@ -20,40 +22,55 @@ void calculate_lethal_dose() {
   const double mouse_mass = 35.0;
   // proportion of sweetener to mass needed to be lethal
   const double mouse_lethal = mouse_sweetener / mouse_mass;
-  // 100lb person mass reference in grams 
+  // 100lb person mass reference in grams
   const double person_mass = 45400.0;
   // soda-can mass in grams
   const double soda_mass = 350.0;
   // amount of sweetener in a soda can
   const double sweetener = 0.001;
 
-  // user-inputted desired weight after dieting
-  double desired_weight;
-  double weight_in_grams;
-  double person_lethal;
-  double soda_lethal;
-  int soda_cans;
-  cout << "Please enter your desired weight:" << endl;
+  bool start_again = true; // keep looping until user exits program
+  string confirm; // confirm whether user wants to rerun or quit program
+  double desired_weight; // user-inputted desired weight after dieting
+  double weight_in_grams; // desired weight converted to grams
+  double person_lethal; // lethal amount of sweetener in grams for desired weight
+  double soda_lethal; // lethal amount of soda in grams
+  int soda_cans; // number of soda cans contain lethal soda amount
+
   // loop until no more input
-  while (cin >> desired_weight) {
-	  // convert the desired weight into grams based on hundred-pound person ratio
-	  weight_in_grams = (desired_weight / 100) * person_mass;
-	  // calculate lethal dose of sweetener in grams needed for person, based on mouse lethal proportion
-	  person_lethal = weight_in_grams * mouse_lethal;
-	  // find lethal dose in grams of soda
-	  soda_lethal = person_lethal / sweetener;
-	  // find lethal dose in cans of soda
-	  soda_cans = soda_lethal / soda_mass;
+  while (start_again) {
+    cout << "Please enter your desired weight:" << endl;
+    cin >> desired_weight;
+    // convert the desired weight into grams based on hundred-pound person ratio
+    weight_in_grams = (desired_weight / 100) * person_mass;
+    // calculate lethal dose of sweetener in grams needed for person, based on mouse lethal proportion
+    person_lethal = weight_in_grams * mouse_lethal;
+    // find lethal dose in grams of soda
+    soda_lethal = person_lethal / sweetener;
+    // find lethal dose in cans of soda
+    soda_cans = soda_lethal / soda_mass;
 
-	  cout << "Lethal dose of sweetener for " << desired_weight << "lbs person: " << person_lethal << "grams of sweetener" << endl;
-	  cout << setprecision(2) << fixed << "You would need to drink about " << soda_cans << " cans or about " << soda_lethal << " grams of soda before you would die trying to diet to " << desired_weight << "lbs" << endl;
+    cout << "Lethal dose of sweetener for " << desired_weight << "lbs person: " << person_lethal << "grams of sweetener" << endl;
+    cout << setprecision(2) << fixed << "You would need to drink about " << soda_cans << " cans or about " << soda_lethal << " grams of soda before you would die trying to diet to " << desired_weight << "lbs" << endl;
 
-	  cout << endl;
-	  cout << "Enter a new weight or exit the program using 'Ctrl+Z' or 'Ctrl+D'" << endl;
+    cout << endl;
+
+    // prompt the user to enter a new desired weight or quit the program
+    cout << "Would you like to enter a new weight? (Y / n)" << endl;
+    while (cin >> confirm) {
+      if (confirm == "Y") {
+        break;
+      } else if (confirm == "n") {
+        start_again = false;
+        break;
+      } else {
+        cout << "Would you like to enter a new weight? (Y / n)" << endl;
+      }
+    }
   }
 }
 
 int main() {
-	calculate_lethal_dose();
+  calculate_lethal_dose();
   return 0;
 }
