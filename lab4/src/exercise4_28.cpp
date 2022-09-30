807bfea2735350dcdf2319d1f7ff20d1582940dd
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-09-29 21:33:15 -0400

    complete source code for exercise 4.28

--- lab4/src/exercise4_28.cpp
+++ lab4/src/exercise4_28.cpp
@@ -1,3 +1,7 @@
+/*
+ * Program to print the size of each built-in type
+ */
+
 #include <iostream>
 
 using std::cin;
@@ -6,10 +10,18 @@ using std::endl;
 
 void size_of_types() {
   cout << "These are the sizes of the built-in types" << endl;
-  cout << "Size of char: " << sizeof(char) << " byte" << endl;
-  cout << "Size of int: " << sizeof(int) << " bytes" << endl;
-  cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-  cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+  cout << "Size of boolean: " << sizeof(bool) << " byte(s)" << endl;
+  cout << "Size of char: " << sizeof(char) << " byte(s)" << endl;
+  cout << "Size of unsigned char: " << sizeof(unsigned char) << " byte(s)" << endl;
+  cout << "Size of signed char: " << sizeof(signed char) << " byte(s)" << endl;
+  cout << "Size of short: " << sizeof(short) << " byte(s)" << endl;
+  cout << "Size of unsigned: " << sizeof(unsigned) << " byte(s)" << endl;
+  cout << "Size of signed: " << sizeof(signed) << " byte(s)" << endl;
+  cout << "Size of int: " << sizeof(int) << " byte(s)" << endl;
+  cout << "Size of float: " << sizeof(float) << " byte(s)" << endl;
+  cout << "Size of double: " << sizeof(double) << " byte(s)" << endl;
+  cout << "Size of long long: " << sizeof(long long) << " byte(s)" << endl;
+  cout << "Size of long double: " << sizeof(long double) << " byte(s)" << endl;
 }
 
 int main() {
