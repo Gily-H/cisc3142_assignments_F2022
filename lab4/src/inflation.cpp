d18eed9fc788dec041c61f3719f5ad4ad28704b4
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-09-29 21:56:29 -0400

    complete source code for lab4 inflation exercise

--- lab4/src/inflation.cpp
+++ lab4/src/inflation.cpp
@@ -1,8 +1,15 @@
+/*
+ * Program to output the cost of an item after adjusting for inflation
+ */
+
 #include <iostream>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::fixed;
+using std::setprecision;
 
 double price_after_inflation() {
   double item_cost;
@@ -12,21 +19,32 @@ double price_after_inflation() {
   cout << "Enter the initial cost of the item:" << endl;
   cin >> item_cost;
 
-  cout << "Enter number of years before intended purchase year:" << endl;
+  cout << "Enter number of years before intended purchase date:" << endl;
   cin >> years_till_purchase;
 
-  cout << "Enter the inflation rate" << endl;
+  cout << "Enter the inflation rate percentage without the (%) symbol" << endl;
   cin >> inflation_rate;
 
-  // convert percentage to a fraction - divide by 100
-  // while (years_till_purchase != 0)
-    // updated cost = item_cost * (1 + inflation_rate);
+  cout << "Initial cost: $" << item_cost << ", Years before purchase: " << years_till_purchase << " years, Inflation rate: " << inflation_rate << "%" << endl;
+
+  // convert the inflation rate percentage to a decimal fraction
+  inflation_rate = inflation_rate / 100;
+
+  unsigned year_count = 0;
+  double updated_cost = item_cost;
+  // calculate the updated cost after adjusting for inflation every year
+  while (year_count != years_till_purchase) {
+	  // calculate the updated cost based on the previous years cost
+	  updated_cost = updated_cost * (1 + inflation_rate);
+	  ++year_count;
+  }
     
-  return item_cost;
+  cout << endl;
+  return updated_cost;
 }
 
 int main() {
   double updated_cost = price_after_inflation();
-  cout << "Cost after inflation is: " << updated_cost << endl; 
+  cout << setprecision(2) << fixed << "Cost after inflation: $" << updated_cost << endl; 
   return 0;
 }
