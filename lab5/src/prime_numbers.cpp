200fa4dd4140d76240efbe2b891f6e3e49f12596
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-10-13 22:17:56 -0400

    complete practice problem prime numbers

---
+++ lab5/src/prime_numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/* 
+ * Program to list out all the prime numbers between 3 and 100 inclusive
+ */
+
+// helper method to check if a number is prime
+bool check_if_prime(int val) {
+  // loop through all factors of the given integer param from 2 (inclusive) to the param value (not inclusive)
+  for (int i = 2; i < val; ++i) {
+    // a value with 0 remainder is not prime
+    if (val % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  cout << "These are all the prime numbers between 3 and 100 inclusive" << endl;
+  for (int i = 3; i <= 100; ++i) {
+    // check if the current number is prime
+    if (check_if_prime(i)) {
+      cout << i << " "; // if prime, output the number
+    }
+  }
+
+  cout << endl;
+}
